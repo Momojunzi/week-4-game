2018816ac21d2b61140e21f57e6f5dffb095b87c
Momojunzi <mark.holthaus@gmail.com>
2017-08-06 19:48:02 -0700

    able to choose player and defender

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,50 @@
 $(document).ready(function() {
-
-	var characterArr = [
-						{name: "Luke Skywalker",
-						 hp: Math.floor((Math.random() * 100) + 100)}, 
-						{name: "ObiWan Kenobi",
-						 hp: Math.floor((Math.random() * 100) + 100)}, 
-						{name: "Darth Vader",
-						 hp: Math.floor((Math.random() * 100) + 100)}, 
-						{name: "Darth Sidious",
-			 			 hp: Math.floor((Math.random() * 100) + 100)}, 
-						{name: "Yoda",
-						 hp: Math.floor((Math.random() * 100) + 100)}, 						
-						 {name: "Count Dooku",
-						 hp: Math.floor((Math.random() * 100) + 100)}
-						];
+	var characterArr = ["Luke Skywalker", "ObiWan Kenobi", "Darth Vader", "Darth Sidious", "Yoda", "Count Dooku",];
 	var playerChosen = false;
+	var defenderChosen = false;
 	var playerChar;
+	var defender;
 	var enemyArr = characterArr;
-
-	/*for(var i=0; i<characterArr.length; i++) {
-		var charDiv = $('<div class="char-img-div col-xs-1" id="' + characterArr[i].split(' ').join('-') + '">');
-		var charImage = $('<img>').attr({src: './assets/images/' + characterArr[i] + '.jpg', class: 'char-img img-responsive'});
-		charDiv.append(charImage)
-		$('#char-start-div').append(charDiv);
-	}*/
+	var playerhp;
+	var defenderhp; 
 
 	arrToDiv(characterArr, "#char-start-div");
-
-
+	
 	$('.char-img-div').on('click', function() {
+
 		if(playerChosen === false) {
 			var charIndex = characterArr.indexOf($(this).attr('id').split('-').join(' '));
 			playerChar = enemyArr.splice(charIndex, 1);
-			arrToDiv(playerChar, "#player-char-div");
-			arrToDiv(enemyArr, "#enemy-div");
+			arrToDiv(playerChar, "#player-char-div", "player");
+			arrToDiv(enemyArr, "#enemy-div", "enemy");
 			$('#char-start-div').css({display: 'none'});
 			$('#player-char-div, #enemy-div').css({display: 'block'});
-			playerChosen === true;
-			console.log(characterArr, playerChar, enemyArr, playerChar[0].hp);
+			playerhp = $('.player').data('hp');
+			playerChosen = true;
 		} 
+
+		$('.enemy').on('click', function() {
+		
+			if(defenderChosen === false) {
+				var charIndex = enemyArr.indexOf($(this).attr('id').split('-').join(' '));
+				defender = enemyArr.splice(charIndex, 1);
+				$('#enemy-div').empty();
+				arrToDiv(defender, '#defender-div', 'defender');
+				arrToDiv(enemyArr, "#enemy-div", 'enemy');
+				$('#defender-div').css({display: 'block'});
+				defenderhp = $('.defender').data('hp');
+				defenderChosen = true;
+				console.log(playerhp, defenderhp);
+			}
+		});
 	});
 
-	function arrToDiv(arr, parent) {
+	function arrToDiv(arr, parent, team) {
+
 		for(var i=0; i<arr.length; i++) {
-			var charDiv = $('<div class="char-img-div col-xs-1" id="' + arr[i].name.split(' ').join('-') + '">');
-			var charImage = $('<img>').attr({src: './assets/images/' + arr[i].name + '.jpg', class: 'char-img img-responsive'});
+			var charDiv = $('<div class="char-img-div col-xs-1 ' + team + '" id="' + arr[i].split(' ').join('-') + '">');
+			charDiv.data('hp', Math.floor((Math.random() * 100) + 100));
+			var charImage = $('<img>').attr({src: './assets/images/' + arr[i] + '.jpg', class: 'char-img img-responsive'});
 			charDiv.append(charImage);
 			$(parent).append(charDiv);
 		}
